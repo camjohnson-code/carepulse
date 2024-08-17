70a74ed66ce6cf0b6a573385dc680570576ef37b
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-08-17 11:44:58 -0600

    Add select case

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -19,6 +19,7 @@ import PhoneInput from 'react-phone-number-input';
 import DatePicker from 'react-datepicker';
 
 import 'react-datepicker/dist/react-datepicker.css';
+import { Select, SelectContent, SelectTrigger, SelectValue } from './ui/select';
 
 interface CustomProps {
   control: Control<any>;
@@ -109,6 +110,22 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
 
     case FormFieldType.SKELETON:
       return renderSkeleton ? renderSkeleton(field) : null;
+
+    case FormFieldType.SELECT:
+      return (
+        <FormControl>
+          <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <FormControl>
+              <SelectTrigger className='shad-select-trigger'>
+                <SelectValue placeholder={placeholder} />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent className='shad-select-content'>
+              {props.children}
+            </SelectContent>
+          </Select>
+        </FormControl>
+      );
 
     default:
       break;
