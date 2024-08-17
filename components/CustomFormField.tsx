b79ba5ce73ddb5f6fc5f279fae8e978edca3d9a1
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-08-17 11:50:47 -0600

    Add text area case

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -20,6 +20,7 @@ import DatePicker from 'react-datepicker';
 
 import 'react-datepicker/dist/react-datepicker.css';
 import { Select, SelectContent, SelectTrigger, SelectValue } from './ui/select';
+import { Textarea } from './ui/textarea';
 
 interface CustomProps {
   control: Control<any>;
@@ -127,7 +128,19 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
         </FormControl>
       );
 
-    default:
+    case FormFieldType.TEXTAREA:
+      return (
+        <FormControl>
+          <Textarea
+            placeholder={placeholder}
+            {...field}
+            className='shad-textArea'
+            disabled={props.disabled}
+          />
+        </FormControl>
+      );
+    
+      default:
       break;
   }
 };
