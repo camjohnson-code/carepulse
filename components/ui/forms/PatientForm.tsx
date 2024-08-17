fa93a95e1a57908cf166b13db7b38791c3bbdef6
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-08-16 23:13:54 -0600

    Refactor form validation

--- components/ui/forms/PatientForm.tsx
+++ components/ui/forms/PatientForm.tsx
@@ -3,9 +3,12 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
-import { Button } from '@/components/ui/button';
 import { Form } from '@/components/ui/form';
 import CustomFormField from '@/components/CustomFormField';
+import SubmitButton from '@/components/SubmitButton';
+import { useState } from 'react';
+import { UserFormValidation } from '@/lib/validation';
+import { useRouter } from 'next/navigation';
 
 export enum FormFieldType {
   INPUT = 'input',
@@ -13,26 +16,33 @@ export enum FormFieldType {
   CHECKBOX = 'checkbox',
 }
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: 'Username must be at least 2 characters.',
-  }),
-});
-
 const PatientForm = () => {
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: '',
+      name: '',
+      email: '',
+      phone: '',
     },
   });
 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+
+    try {
+    //   const userData = { name, email, phone };
+    //   const user = await createUser(userData);
+    //   if (user) router.push(`/patients/${user.$id}/register`);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
@@ -69,7 +79,7 @@ const PatientForm = () => {
           placeholder='(123) 456-7890'
         />
 
-        <Button type='submit'>Submit</Button>
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
   );
