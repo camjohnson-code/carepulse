959c601899dfe6f4cd9efa79c3356a8d3c39597a
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-08-16 23:40:53 -0600

    Update name message

--- lib/validation.ts
+++ lib/validation.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const UserFormValidation = z.object({
   name: z
     .string()
-    .min(2, 'Username must be at least 2 characters.')
+    .min(2, 'Name must be at least 2 characters.')
     .max(50, 'Name must be at most 50 characters.'),
   email: z.string().email('Invalid email address'),
   phone: z
