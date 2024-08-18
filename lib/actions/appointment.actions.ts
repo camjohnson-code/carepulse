3c0288dcf0811a1fa5a71fb6c68133aa228969ef
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-08-18 12:59:44 -0600

    Add getRecentAppointments action

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { ID } from 'node-appwrite';
+import { ID, Query } from 'node-appwrite';
 import {
   APPOINTMENT_COLLECTION_ID,
   DATABASE_ID,
@@ -34,6 +34,34 @@ export const getAppointment = async (appointmentId: string) => {
     );
 
     return parseStringify(appointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getRecentAppointmentList = async () => {
+  try {
+    const appointments = await databases.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      [Query.orderDesc('$createdAt')]
+    );
+
+    const counts = appointments.documents.reduce((acc, appointment) => {
+        if (appointment.status === 'scheduled') acc.scheduledCount += 1;
+        else if (appointment.status === 'pending') acc.pendingCount += 1;
+        else if (appointment.status === 'cancelled') acc.cancelledCount += 1;
+
+        return acc;
+      }, { scheduledCount: 0, pendingCount: 0, cancelledCount: 0 });
+
+    const data = {
+      totalCount: appointments.total,
+      ...counts,
+      documents: appointments.documents,
+    };
+
+    return parseStringify(data);
   } catch (error) {
     console.log(error);
   }
