6021bc9935de9a720a9a541d380aa58e74ad11ba
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-08-18 18:46:33 -0600

    Add sms feature

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -5,8 +5,9 @@ import {
   APPOINTMENT_COLLECTION_ID,
   DATABASE_ID,
   databases,
+  messaging,
 } from '../appwrite.config';
-import { parseStringify } from '../utils';
+import { formatDateTime, parseStringify } from '../utils';
 import { revalidatePath } from 'next/cache';
 
 export const createAppointment = async (
@@ -87,8 +88,30 @@ export const updateAppointment = async ({
 
     if (!updatedAppointment) throw new Error('Appointment not found');
 
+    const formattedDateTime = formatDateTime(appointment.schedule).dateTime;
+    const smsMessage = `Hi, it's CarePulse! Your appointment on ${formattedDateTime} has been ${type}${
+      type === 'schedule' ? 'd' : 'ed'
+    }.`;
+
+    await sendSMSNotification(userId, smsMessage);
+
     revalidatePath('/admin');
     return parseStringify(updatedAppointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const sendSMSNotification = async (userId: string, content: string) => {
+  try {
+    const message = await messaging.createSms(
+      ID.unique(),
+      content,
+      [],
+      [userId]
+    );
+
+    return parseStringify(message);
   } catch (error) {
     console.log(error);
   }
